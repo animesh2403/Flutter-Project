59b186170923b8b1f03f0ae6196c14ad6dbfd747
animesh2403 <animeshmishra2403@gmail.com>
2021-06-13 13:57:38 +0530

    First Commit

---
+++ lib/Screens/FirstScreen.dart
@@ -0,0 +1,42 @@
+import 'package:demo/Screens/SecondScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:demo/Screens/FirstScreen.dart';
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 350,),
+              Text('This is First screen'),
+              SizedBox(height: 30,),
+              ElevatedButton(onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder:(context){
+                  return SecondScreen();
+                }));
+              }, child: Text('Go to 2nd screen')
+              ),
+              SizedBox(height: 30),
+              InkWell(
+                child: Icon(Icons.arrow_back) ,
+                onTap: (){
+                  Navigator.pop(context);
+                }
+              ),
+            ],
+          ),
+
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,91 @@
+import 'package:demo/Screens/FirstScreen.dart';
+import 'package:demo/Screens/SecondScreen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-        drawer: Drawer(
-            child: Column(
-              children: [
-                Container(
-                  child: Center(
-                    child: Column(
-                      children: [
-                        Container(
-                          width: 100,
-                          height: 200,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.rectangle,
-                            image: DecorationImage(image: NetworkImage(
-                                'https://yt3.ggpht.com/ytc/AAUvwng8vsKs2CDl14rjq7nArGUE5Aiuup5g0tn9JPtuHfM=s900-c-k-c0x00ffffff-no-rj')),
-                          ),
-                        ),
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MY DEMO',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-                      ],
-                    ),
-                  ),
-                ),
-                ListTile(
-                  leading: Icon(Icons.person),
-                  title: Text(
-                      'Profile',style: TextStyle(fontSize: 18),),
-                  onTap: null,
-                ),
-                ListTile(
-                  leading: Icon(Icons.settings),
-                  title: Text(
-                    'Settings',style: TextStyle(fontSize: 18),),
-                  onTap: null,
-                ),
-                ListTile(
-                  leading: Icon(Icons.logout),
-                  title: Text(
-                    'Log Out',style: TextStyle(fontSize: 18),),
-                  onTap: null,
-                ),
-              ],
-          ),
-        ),
-        appBar: AppBar(
-          actions: [
-            Icon(Icons.settings),
-            SizedBox(width: 20,),
-            Icon(Icons.logout),
-          ],
-          title: Text('My app'),
-          centerTitle: true,
-          backgroundColor: Colors.deepPurple,
-        ),//appBar: ,
-        body: ListView(
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        child: Column(
           children: [
+            Container(
+              child: Center(
+                child: Column(
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        image: DecorationImage(image: NetworkImage(
+                            'https://yt3.ggpht.com/ytc/AAUvwng8vsKs2CDl14rjq7nArGUE5Aiuup5g0tn9JPtuHfM=s900-c-k-c0x00ffffff-no-rj')),
+                      ),
+                    ),
+
+                  ],
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text(
+                'Profile',style: TextStyle(fontSize: 18),),
+              onTap: null,
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text(
+                'Settings',style: TextStyle(fontSize: 18),),
+              onTap: null,
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text(
+                'Log Out',style: TextStyle(fontSize: 18),),
+              onTap: null,
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        actions: [
+          Icon(Icons.settings),
+          SizedBox(width: 20,),
+          Icon(Icons.logout),
+        ],
+        title: Text('My app'),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+      ),//appBar: ,
+      body: ListView(
+        children: [
           Center(
             child: Column(
-              children: [
+              children:<Widget>[
                 TextField(
                   decoration: InputDecoration(
                     hintText: 'Enter Your Name',
@@ -77,25 +102,40 @@ void main() => runApp(MaterialApp(
                 Image.asset('assets/flutter.jpg',width: 200,height: 200,),
                 Text('This is Network Image',style: TextStyle(fontSize: 20),),
                 Image.network('https://yt3.ggpht.com/ytc/AAUvwng8vsKs2CDl14rjq7nArGUE5Aiuup5g0tn9JPtuHfM=s900-c-k-c0x00ffffff-no-rj',width: 200,height: 200,),
-                ElevatedButton(onPressed: null, child: Text('Elevated Button')),
+                ElevatedButton(onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder:(context){
+                    return FirstScreen();
+                  }));
+                }, child: Text('Go to 1st screen')),
               ],
             ),
           ),],
-        ),
-        floatingActionButton:Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            FloatingActionButton(
-              onPressed: (){},
-              backgroundColor: Colors.black,
-              child: Icon(Icons.add),
-            ),
-            FloatingActionButton(
-                onPressed: (){},
-                backgroundColor: Colors.cyanAccent,
-              child: Icon(Icons.call),
-            ),
-          ],
-        ),
-    ),
-));
+      ),
+      floatingActionButton:Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(width: 15),
+          FloatingActionButton(
+            onPressed: (){},
+            backgroundColor: Colors.black,
+            child: Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: (){},
+            backgroundColor: Colors.cyanAccent,
+            child: Icon(Icons.call),
+          ),
+          SizedBox(
+            width: 200,
+          ),
+          InkWell(
+              child: Icon(Icons.exit_to_app_rounded) ,
+              onTap: (){
+                Navigator.pop(context);
+              }
+          ),
+        ],
+      ),
+    );
+  }
+}
